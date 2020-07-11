6b7872075cf432cebb6597516d18614eccc0e1d2
Brian2524 <b2hinkle@gmail.com>
2020-07-10 20:00:02 -0400

    Implement HostModel and GameInstanceModel and create Game enum

--- MultiplayerCentralServices/MultiplayerFrameworkModelsLibrary/Models/GameInstanceModel.cs
+++ MultiplayerCentralServices/MultiplayerFrameworkModelsLibrary/Models/GameInstanceModel.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace MultiplayerFrameworkModelsLibrary.Models
 {
+    public enum Game
+    {
+        None,
+        NinjaGame
+    }
     class GameInstanceModel
     {
+        public int Id { get; set; }
+        public Game Game { get; set; }
+        public string Args { get; set; }
+        public string AssociatedServer { get; set; }
     }
 }
--- MultiplayerCentralServices/MultiplayerFrameworkModelsLibrary/Models/HostModel.cs
+++ MultiplayerCentralServices/MultiplayerFrameworkModelsLibrary/Models/HostModel.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace MultiplayerFrameworkModelsLibrary.Models
 {
+    /*A Host should have 2 Ip addresses, one reserved for the GameInstances to listen on for players (HostIp),
+      and another for the HostServicesApi to listen on (AssociatedHostServicesApiIp). This way, players don't get access to the Api.*/
     class HostModel
     {
+        public int Id { get; set; }
+        public string HostIp { get; set; }
+        public string AssociatedHostServicesApiIp { get; set; }
+        public string AssociatedHostServicesApiPort { get; set; }
+        public bool IsActive { get; set; }
     }
 }
