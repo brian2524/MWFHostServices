c6e9faf3850ff8bd5b39eba41ba8d766faa3b230
Brian2524 <b2hinkle@gmail.com>
2020-07-13 02:18:46 -0400

    Setup dependency injection for DatabaseServicesAPIFunctionApp through a Startup class. Also setup HttpClientFactory for the Azure funtions to be able to use the HttpClientFactory's pooling system.

--- MultiplayerCentralServices/DatabaseServicesAPIFunctionApp/SampleController.cs
+++ MultiplayerCentralServices/DatabaseServicesAPIFunctionApp/SampleController.cs
@@ -7,16 +7,25 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
+using System.Net.Http;
 
 namespace DatabaseServicesAPIFunctionApp
 {
-    public static class SampleController
+    public class SampleController
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SampleController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        }
+
         [FunctionName("SampleEndpoint")]
-        public static async Task<IActionResult> SampleEndpoint(
+        public async Task<IActionResult> SampleEndpoint(
             [HttpTrigger(AuthorizationLevel.Admin, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
@@ -31,6 +40,8 @@ namespace DatabaseServicesAPIFunctionApp
             JsonElement jsonBody = (JsonElement)(JsonSerializer.Deserialize<Object>(requestBody));
 
             name = name ?? jsonBody.GetProperty("name").ToString();
+
+                                                                         //  HttpClient httpClient = _httpClientFactory.CreateClient();                  Sample creation of an HttpClient to use for calling APIs like our HostSerivces APIc
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
---
+++ MultiplayerCentralServices/DatabaseServicesAPIFunctionApp/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(DatabaseServicesAPIFunctionApp.Startup))]
+
+namespace DatabaseServicesAPIFunctionApp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            //  Basic HTTP client factory usage
+            builder.Services.AddHttpClient();
+        }
+    }
+}
