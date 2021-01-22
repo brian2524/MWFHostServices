5d8d518df64db7685035da2325c67c3bfdb2690f
Brian2524 <b2hinkle@gmail.com>
2021-01-22 17:57:40 -0500

    Add GameModel class. This will be useful for instantiating a game instance from this model (overriding values before instantiation is the plan for ease of use)

---
+++ MultiplayerWebFramework/MWFModelsLibrary/Models/GameModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MWFModelsLibrary.Models
+{
+    class GameModel
+    {
+        public string Name { get; set; }
+        //  This should be enum instead with dropdown for choosing
+        public string Game { get; set; }
+        public string Map { get; set; }
+        public string Args { get; set; }
+    }
+}
