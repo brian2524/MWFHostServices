8fb81e218e8c4f9082697bbcf0ca0ac2f99d26aa
Brian2524 <b2hinkle@gmail.com>
2021-02-07 17:19:29 -0500

    add comment

--- MultiplayerWebFramework/MWFModelsLibrary/Models/GameInstanceModel.cs
+++ MultiplayerWebFramework/MWFModelsLibrary/Models/GameInstanceModel.cs
@@ -11,6 +11,7 @@ namespace MWFModelsLibrary.Models
         public int Id { get; set; }
         public Game Game { get; set; }
         public string Args { get; set; }
+        // Maybe implement is so this will be the associated host Id so we can do join statements
         public string AssociatedHost { get; set; }
     }
 }
