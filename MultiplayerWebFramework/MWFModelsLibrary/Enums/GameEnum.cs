eebdeca03b26dcdb671f03c34e7f7b8b40f48532
Brian2524 <b2hinkle@gmail.com>
2021-03-24 21:08:04 -0400

    Change game names to start from 0 for simplification

--- MultiplayerWebFramework/MWFModelsLibrary/Enums/GameEnum.cs
+++ MultiplayerWebFramework/MWFModelsLibrary/Enums/GameEnum.cs
@@ -6,10 +6,10 @@ namespace MWFModelsLibrary.Enums
 {
     public enum Game
     {
+        Game0,
         Game1,
         Game2,
         Game3,
-        Game4,
-        Game5
+        Game4
     }
 }
