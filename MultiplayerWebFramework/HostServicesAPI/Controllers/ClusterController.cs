b75cf680fda5c0a319d7e03ff61caa507723b70c
Brian2524 <b2hinkle@gmail.com>
2021-04-03 23:38:46 -0400

    Make enum parsing system for json property "Game"

--- MultiplayerWebFramework/HostServicesAPI/Controllers/ClusterController.cs
+++ MultiplayerWebFramework/HostServicesAPI/Controllers/ClusterController.cs
@@ -16,22 +16,38 @@ namespace HostServicesAPI.Controllers
     [ApiController]
     public class ClusterController : ControllerBase
     {
-        private readonly IHttpClientFactory _clientFactory;
+/*        private readonly IHttpClientFactory _clientFactory;
         public ClusterController(IHttpClientFactory clientFactory)
         {
             _clientFactory = clientFactory;
-        }
+        }*/
 
         [HttpPost]
         public GameInstanceModel SpinUp([FromBody] JsonElement req)
         {
-            Game    reqGame   = (Game)(req.GetProperty("Game").GetInt32());
-            string  reqArgs   = req.GetProperty("Args").GetString();
+            // This was a system I was working on so that the endpoint can accept the "Game" parameter as an int or string and it will just parse what you sent
+            /*Game reqGameCasted = Game.Game0;
+            if (req.GetProperty("Game").TryGetInt32(out int reqGameInt))
+            {
+                reqGameCasted = (Game)reqGameInt;
+            }
+            else
+            {
+                string reqGameString = req.GetProperty("Game").GetString();
+                if (Enum.TryParse(typeof(Game), reqGameString, out object game))
+                {
+                    reqGameCasted = (Game)game;
+                }
+
+            }*/
+
+            Game reqGameCasted = (Game)(req.GetProperty("Game").GetInt32());
+            string reqArgs     = req.GetProperty("Args").GetString();
             // Request will only give us the game to start and the arguments when starting it. Everything else will be decided by the Host (us)
 
             // This is BAD! this is only for testing (maybe add a cluster as a singleton in ConfigureServices instead)
             Cluster cluster = new Cluster();
-            GameInstanceModel newGameInstance = cluster.SpinUp(reqGame, reqArgs);
+            GameInstanceModel newGameInstance = cluster.SpinUp(reqGameCasted, reqArgs);
 
             if (newGameInstance == null)
             {
