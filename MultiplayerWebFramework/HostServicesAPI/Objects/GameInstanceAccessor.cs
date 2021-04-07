7cf18a4d1640ed1e5431ae688a5ef9eacc42220d
Brian2524 <b2hinkle@gmail.com>
2021-04-07 06:10:22 -0400

    Create GameInstanceAccessor class to soon be the array type the cluster holds

---
+++ MultiplayerWebFramework/HostServicesAPI/Objects/GameInstanceAccessor.cs
@@ -0,0 +1,15 @@
+﻿using MWFModelsLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HostServicesAPI.Objects
+{
+    public class GameInstanceAccessor
+    {
+        public Process gameInstanceProcess { get; set; }
+        public GameInstanceModel gameInstanceDatabaseModel { get; set; }
+    }
+}
