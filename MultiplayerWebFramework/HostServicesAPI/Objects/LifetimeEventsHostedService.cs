9948c131034f379c6a672e7175b49308d71cb858
Brian2524 <b2hinkle@gmail.com>
2021-04-07 16:32:55 -0400

    Add a host service for lifetime events

---
+++ MultiplayerWebFramework/HostServicesAPI/Objects/LifetimeEventsHostedService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HostServicesAPI.Objects
+{
+    internal class LifetimeEventsHostedService : IHostedService
+    {
+        private readonly IHostApplicationLifetime _appLifetime;
+
+        public LifetimeEventsHostedService(IHostApplicationLifetime appLifetime)
+        {
+            _appLifetime = appLifetime;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _appLifetime.ApplicationStarted.Register(OnStarted);
+            _appLifetime.ApplicationStopping.Register(OnStopping);
+            _appLifetime.ApplicationStopped.Register(OnStopped);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private void OnStarted()
+        {
+            // Perform post-startup activities here
+        }
+
+        private void OnStopping()
+        {
+            // Perform on-stopping activities here
+        }
+
+        private void OnStopped()
+        {
+            // Perform post-stopped activities here
+        }
+    }
+}
+
